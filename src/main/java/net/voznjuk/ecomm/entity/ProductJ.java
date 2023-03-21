03429c0e09f068ffbb2639a1dcb035cc7d5672b7
Kostiantyn Vozniuk <kvoznjuk@gmail.com>
2023-03-21 23:08:14 +0200

    First

---
+++ src/main/java/net/voznjuk/ecomm/entity/ProductJ.java
@@ -0,0 +1,40 @@
+package net.voznjuk.ecomm.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product_j")
+@Data
+public class ProductJ {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="price")
+    private BigDecimal price;
+
+    @Column(name="status")
+    private boolean active;
+
+    @Column(name="quontity")
+    private int unitInStock;
+
+    @Column(name="created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name="last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
